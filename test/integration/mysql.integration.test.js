f018ad020ef2ba75e44fab895a0689056ec61a25
Justin Durham <justin.durham@icloud.com>
2016-11-29 13:28:11 -0500

    Updates

--- test/integration/mysql.integration.test.js
+++ test/integration/mysql.integration.test.js
@@ -4,34 +4,43 @@ import should from 'should';
 import LoggerMock from './logger-mock';
 import _ from 'lodash';
 
-const logger = new LoggerMock();
-
-const rdsConf = config.get('aeg-mysql');
-
 describe('MySQL', async () => {
 
-	const options = {
-		logger,
-		connectionLimit: 10,
-		host: rdsConf.host,
-		user: rdsConf.user,
-		password: rdsConf.password,
-		database: rdsConf.db,
-		insecureAuth: true,
-		acquireTimeout: 120000,
-		waitForConnections: true,
-		queueLimit: 0,
-		timezone: 'Z'
-	};
+	let mysql = null;
+	let options = null;
 
-	const mysql = new MySQL(options);
+	before(() => {
+
+		const logger = new LoggerMock();
+		const rdsConf = config.get('aeg-mysql');
+		options = {
+			logger,
+			connectionLimit: 10,
+			host: rdsConf.host,
+			user: rdsConf.user,
+			password: rdsConf.password,
+			database: rdsConf.db,
+			insecureAuth: true,
+			acquireTimeout: 120000,
+			waitForConnections: true,
+			queueLimit: 0,
+			timezone: 'Z'
+		};
+
+		mysql = new MySQL(options);
+
+	});
+
+	after(async () => {
+
+		await mysql.dispose();
+
+	});
 
 	it('tables', async () => {
 
 		const result = await mysql.tables('hitpath_import');
 		should.exist(result);
-
-		// console.log(result);
 
 	});
 
@@ -40,8 +49,6 @@ describe('MySQL', async () => {
 		const result = await mysql.query('SELECT * FROM hitpath_affiliates LIMIT 10');
 		should.exist(result);
 
-		// console.log(result);
-
 	});
 
 	it('queryAll', async () => {
@@ -49,15 +56,13 @@ describe('MySQL', async () => {
 		const result = await mysql.queryAll('hitpath_import', 'hitpath_affiliates');
 		should.exist(result);
 
-		// console.log(result);
-
 	});
 
 	it('queryStream', async () => {
 
 		await mysql.queryStream('SELECT * FROM hitpath_affiliates LIMIT 10', (record) => {
 
-			// console.log(record);
+			should.exist(record);
 
 		});
 
@@ -70,19 +75,63 @@ describe('MySQL', async () => {
 		result.should.be.a.Number;
 		result.should.be.greaterThan(0);
 
-		// console.log(result);
+	});
+
+	it('clear test', async () => {
+
+		await mysql.query('truncate table node_test.test_1');
 
 	});
 
 	it('writeRecord', async () => {
 
-		await mysql.writeRecord('node_test', 'test_1', {id: Math.random(), name: 'test'});
+		await mysql.writeRecord('node_test', 'test_1', {id: 0, name: 'test'});
+
+	});
+
+	it('withTransaction - success', async () => {
+
+		await mysql.withTransaction(async (connection) => {
+
+			await mysql.writeRecord('node_test', 'test_1', {id: 2, name: 'test2'}, {connection});
+			await mysql.writeRecord('node_test', 'test_1', {id: 3, name: 'test3'}, {connection});
+			await mysql.writeRecord('node_test', 'test_1', {id: 4, name: 'test4'}, {connection});
+
+		});
+
+		const result = await mysql.queryAll('node_test', 'test_1');
+		should.exist(result);
+		result.length.should.be.equal(4);
+
+	});
+
+	it('withTransaction - fail', async () => {
+
+		try {
+
+			await mysql.withTransaction(async (connection) => {
+
+				await mysql.writeRecord('node_test', 'test_1', {id: 5, name: 'test5'}, {connection});
+				await mysql.writeRecord('node_test', 'test_1', {id: 6, name: 'test6'}, {connection});
+				await mysql.writeRecord('node_test', 'test_1', {id: 7, name: 'test7'}, {connection});
+				throw new Error('kill it');
+
+			});
+
+		} catch (ex) {
+
+			// should be invoked
+
+		}
+
+		const result = await mysql.queryAll('node_test', 'test_1');
+		should.exist(result);
+		result.length.should.be.equal(4);
 
 	});
 
 	it('withConnection', async () => {
 
-		// noinspection JSCheckFunctionSignatures
 		await MySQL.withConnection(async (mysql) => {
 
 			const result = await mysql.queryAll('hitpath_import', 'hitpath_affiliates');
