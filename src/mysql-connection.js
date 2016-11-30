237be571efb98745e44d709cdad261b1832cfbb7
Justin Durham <justin.durham@icloud.com>
2016-11-30 08:22:11 -0500

    updates

---
+++ src/mysql-connection.js
@@ -0,0 +1,220 @@
+import mysql from 'mysql';
+import Promise from 'bluebird';
+import MySQL from './mysql';
+import actions from './actions';
+
+/**
+ * Manages MySQL
+ */
+class MySQLConnection extends MySQL {
+
+	/**
+	 * Constructor
+	 * @param {Object} [options]
+	 */
+	constructor (options = {}) {
+
+		super(options);
+
+		if (options.connection) {
+
+			this._connection = options.connection;
+
+		} else {
+
+			this._connection = mysql.createConnection(options);
+
+		}
+
+	}
+
+	/**
+	 * Begin a transaction
+	 */
+	async begin () {
+
+		return actions.begin(this._connection);
+
+	}
+
+	/**
+	 * Commit a transaction
+	 */
+	async commit () {
+
+		return actions.commit(this._connection);
+
+	}
+
+	/**
+	 * Rollback a transaction
+	 */
+	async rollback () {
+
+		return actions.rollback(this._connection);
+
+	}
+
+	/**
+	 * Perform queries on a connection
+	 * @param {function} delegate
+	 * @param {{noAutoCommit: boolean}} [options]
+	 */
+	static async withConnection (delegate, options = {}) {
+
+		const mysqlConnection = new MySQLConnection(options);
+
+		if (options.noAutoCommit) {
+
+			await mysqlConnection.query('SET autocommit=0');
+
+		}
+
+		await Promise.resolve(delegate(mysqlConnection));
+
+		if (options.noAutoCommit) {
+
+			await mysqlConnection.query('COMMIT');
+
+		}
+
+		await mysqlConnection.dispose();
+
+	}
+
+	/**
+	 * Perform queries within a transaction
+	 * @param {function} delegate
+	 * @param {Object} [options]
+	 */
+	static async withTransaction (delegate, options = {}) {
+
+		const mysqlConnection = new MySQLConnection(options);
+
+		await mysqlConnection.begin();
+
+		try {
+
+			await Promise.resolve(delegate(mysqlConnection));
+			await mysqlConnection.commit();
+
+		} catch (ex) {
+
+			await mysqlConnection.rollback();
+			throw ex;
+
+		} finally {
+
+			if (!options.connection) {
+
+				await mysqlConnection.dispose();
+
+			}
+
+		}
+
+	}
+
+	/**
+	 * Get the tables in the db
+	 * @param {string} db
+	 */
+	async tables (db) {
+
+		return actions.tables(this._connection, db);
+
+	}
+
+	/**
+	 * Query
+	 * @param {string} query
+	 * @param {Object[]} queryArgs
+	 * @return {Object[]}
+	 */
+	async query (query, queryArgs = []) {
+
+		try {
+
+			return actions.query(this._connection, query, queryArgs);
+
+		} catch (ex) {
+
+			this.error('query', {err: ex});
+			throw ex;
+
+		}
+
+	}
+
+	/**
+	 * Query all the records
+	 * @param {string} db
+	 * @param {string} table
+	 */
+	async queryAll (db, table) {
+
+		return actions.queryAll(this._connection, db, table);
+
+	}
+
+	/**
+	 * Query
+	 * @param {string} query
+	 * @param {function} delegate
+	 * @param {Object} [queryArgs]
+	 */
+	async queryStream (query, delegate, queryArgs = []) {
+
+		return actions.queryStream(this._connection, query, delegate, queryArgs);
+
+	}
+
+	/**
+	 * Count
+	 * @param {string} db
+	 * @param {string} table
+	 * @return {number}
+	 */
+	async count (db, table) {
+
+		return actions.count(this._connection, db, table);
+
+	}
+
+	/**
+	 * Saves by upsert a record converting datetimes to the locale set on the timezone of the server
+	 * @param {string} db
+	 * @param {string} table
+	 * @param {Object} record
+	 */
+	async writeRecord (db, table, record) {
+
+		return actions.writeRecord(this._connection, db, table, record);
+
+	}
+
+	/**
+	 * Saves by upsert a record using date-times with the client locale
+	 * @param {string} db
+	 * @param {string} table
+	 * @param {Object} record
+	 */
+	async writeRecordNoLocale (db, table, record) {
+
+		return actions.writeRecordNoLocale(this._connection, db, table, record);
+
+	}
+
+	/**
+	 * Dispose the connection
+	 */
+	async dispose () {
+
+		await super.dispose();
+		await actions.end(this._connection);
+
+	}
+
+}
+
+export default MySQLConnection;
